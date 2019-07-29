25c9b21ad696a5d0b0c84dc8039ba8532bdbf02e
Anand P S <49161738+anandvfcvijay@users.noreply.github.com>
2019-07-29 19:08:35 +0530

    Update disable.py

--- tg_bot/modules/disable.py
+++ tg_bot/modules/disable.py
@@ -195,7 +195,7 @@ if is_module_loaded(FILENAME):
         return build_curr_disabled(chat.id, chat.id)
 
 
-    __mod_name__ = "Cmd Disabling"
+    __mod_name__ = "Disabling"
 
     __help__ = """
  - /cmds: check the current status of disabled commands
